623ec5fa25f7b91a13a7d3f2f9c47276e0838210
Saad Ghani <saadhku2@gmail.com>
2017-10-04 02:16:17 +0800

    yay its done
    easy peasy deasy weasy

--- 1-3/crypt1.java
+++ 1-3/crypt1.java
@@ -9,40 +9,48 @@ import java.io.*;
 import java.lang.*;
 
 public class crypt1{
-	int count =0;
-	
+	static int count =0;
+	static String setS ="";
 	public static void main(String[] args)throws IOException{
 		BufferedReader in = new BufferedReader(new FileReader("crypt1.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("crypt1.out")));
-		//Scanner in = new Scanner(System.in);
+		
 		int N = Integer.parseInt(in.readLine());
 		StringTokenizer st = new StringTokenizer(in.readLine());
+
 		int[] set = new int[N];
 		for(int i=0;i<N;i++){
 			set[i] = Integer.parseInt(st.nextToken());
+			setS+=set[i];
 		}
+		
 		int[] subList = new int[5];
-
 		solve(set,subList,0);
-		
-		
+		out.println(count);
+		out.close();
 	}
 	
 	static int solve(int[] numList, int[] subList, int pos){
-		for(int ind=0;ind<numList.length;ind++){
+		for(int ind=0; ind<numList.length; ind++){
 			if(pos<5){
 				subList[pos] = numList[ind];
-				pos = solve(numList, subList, ind, ++pos);
+				pos = solve(numList, subList, ++pos);
 			}else{
 				StringBuilder strNum = new StringBuilder();
 				for(int n : subList){
 					strNum.append(n);
 				}
-				int threes = Integer.parseInt(strNum.substring(0,3));
-				int twos = Integer.parseInt(strNum.substring(3,5));
-				int mult = threes*twos;
-				if(mult<=9999 && ){
 
+				int mult = Integer.parseInt(strNum.substring(0,3))*Integer.parseInt(strNum.substring(3,5));
+				int partialM1 = Integer.parseInt(strNum.substring(0,3))*Integer.parseInt(strNum.substring(4,5));
+				int partialM2 = Integer.parseInt(strNum.substring(0,3))*Integer.parseInt(strNum.substring(3,4));
+
+				if(mult<=9999 && partialM1<=999 && partialM2<=999){
+					if(String.valueOf(mult).matches("^(["+setS+"]{4})") && String.valueOf(partialM1).matches("^(["+setS+"]{3})")
+					 && String.valueOf(partialM2).matches("^(["+setS+"]{3})")){
+						//System.out.println(strNum);
+						count++;
+					}
 				}
 				return --pos;
 			}
