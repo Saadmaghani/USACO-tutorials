2758eb7396b0f4da6cf23e96c30f07b298cc15e3
Saad Ghani <saadhku2@gmail.com>
2018-01-11 21:14:27 +0800

    finally
    changed hashset to a array of booleans. made it surprisingly much faster

--- 1-4/ariprog.java
+++ 1-4/ariprog.java
@@ -14,21 +14,25 @@ class ariprog{
 		BufferedReader in = new BufferedReader(new FileReader("ariprog.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ariprog.out")));
 
-		HashSet<Integer> set = new HashSet<Integer>();
 		int N = Integer.parseInt(in.readLine());
 		int M = Integer.parseInt(in.readLine());
+		int high = 2*M*M+10;
+		
+		ArrayList<Integer> aList = new ArrayList<Integer>();
+		boolean[] set = new boolean[high];
+
 		for(int p=0; p<=M; p++){
 			for(int q=p; q<=M; q++){
 				int x = (p*p) + (q*q);
-				set.add(x);
+				if(set[x]) continue;
+				set[x]=true;
+				aList.add(x);
 			}
 		}
-
-		int high = 2*M*M;
-		int maxInd = set.size()-N+1;
-		boolean found = false;
-		ArrayList<Integer> aList = new ArrayList<Integer>(set);
 		Collections.sort(aList);
+
+		int maxInd = high-1;
+		boolean found = false;
 		
 		for(int b=1; b <= (high/(N-1)); b++){
 			for(int i=0; i<aList.size(); i++){
@@ -36,7 +40,7 @@ class ariprog{
 				if( ((N-1)*b +a)<=high && i < maxInd ){
 					boolean seqWhole = true;
 					for(int j=1; j<N; j++){
-						if(!set.contains(a+j*b)){
+						if(!set[a+j*b]){
 							seqWhole=false;
 							break;
 						}
